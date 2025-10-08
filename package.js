aace42a51112d1f7a9a2ae1cf202148442dc6d74
management-project-k <management.project.k@gmail.com>
2025-10-08 20:11:40 +0530

    Update package.js

--- package.js
+++ package.js
@@ -11,11 +11,19 @@
   "dependencies": {
     "react": "^18.3.1",
     "react-dom": "^18.3.1",
-    "next": "^14.2.0",
+    "next": "14.2.15",
     "@calcom/embed-react": "^1.5.0",
-    "@emailjs/browser": "^4.3.3",
+    "@emailjs/browser": "^4.4.1",
     "lottie-react": "^2.4.0",
     "animejs": "^3.2.2",
-    "framer-motion": "^11.3.0"
+    "framer-motion": "^11.11.1"
+  },
+  "devDependencies": {
+    "eslint": "^8.57.1",
+    "eslint-config-next": "14.2.15"
+  },
+  "engines": {
+    "node": ">=18.17.0",
+    "npm": ">=9.0.0"
   }
 }
